300bf642b802aae68c13b0312a808988216d1325
Cphil31 <cecilephilippe31@gmail.com>
2024-02-05 10:44:44 +0100

    update flash

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,15 +12,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(Request $request): Response
     {
-        $session = $request->getSession();
-        $session->set('message','page home');
-        $foo = $session->get('message');
-        $this->addFlash(
-            'notice',
-            $foo
-        );
-
-        unset($_SESSION['foo']);
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Home',
         ]);
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,22 +7,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController
+class ProductController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
+    #[Route('/product', name: 'app_product')]
     public function index(Request $request): Response
     {
         $session = $request->getSession();
-        $session->set('message', 'vous êtes passé sur la page contact');
-        
-        
+        $session->set('message', 'vous êtes passé sur la page produit');
+
         $this->addFlash(
             'notice',
             $session->get('message')
         );
-       
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'Contact',
+
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
         ]);
     }
 }
